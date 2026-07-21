6a9cdc5402e8c1c4c4a40e73d2b89da7d71174d8
MohdMufeet <mohdmufeet786@gmail.com>
2026-07-22 00:52:38 +0530

    feat: add Optional component and enhance HeroSection layout with improved styling

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Logo = () => {
   return (
     <>
-      <p className="text-blue-700 font-bold text-3xl">Saas</p>
+      <p className="text-indigo-600 font-bold text-3xl">Saas</p>
     </>
   );
 };
---
+++ src/components/Optional.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { FaFileCode } from "react-icons/fa";
+import { FaLaptop } from "react-icons/fa6";
+import { PiHandGrabbingLight } from "react-icons/pi";
+const Optional = () => {
+  return (
+    <div className="hidden md:flex gap-6 mt-4">
+        <div className="flex gap-2">
+            <FaFileCode className="text-gray-600 text-xl"/>
+          <p p className='font-thin text-gray-600'>
+            Code optional 
+          </p>
+        </div>
+        <div className="flex gap-2">
+            <PiHandGrabbingLight className="text-gray-600 text-xl"/>
+          <p p className='font-thin text-gray-600'>
+            Drag and drop builder  
+          </p>
+        </div>
+        <div className="flex gap-2">
+            <FaLaptop className="text-gray-600 text-xl"/>
+          <p className='font-thin text-gray-600'>
+            Windows, Mac, Linux 
+          </p>
+        </div>
+    </div>
+  )
+}
+
+export default Optional
