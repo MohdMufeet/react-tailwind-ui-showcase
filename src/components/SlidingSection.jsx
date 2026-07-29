22cd94c872722b6ecb13f950f7b836e5b42ca81c
MohdMufeet <mohdmufeet786@gmail.com>
2026-07-29 23:01:21 +0530

    feat: update text in SlidingSection component from 'HIi' to 'Blogger' and adjust animation translation

--- src/components/SlidingSection.jsx
+++ src/components/SlidingSection.jsx
@@ -6,86 +6,86 @@ const SlidingSection = () => {
     <div className=" w-full bg-gray-900 flex flex-col my-4 gap-4">
 
       <p className='text-center text-2xl text-white my-6 font-bold'>OUR Companies</p>
-        <div className="lin1 flex items-center gap-4 hover:-translate-x-40 -translate-x-64 m-6">
+        <div className="lin1 flex items-center gap-4 hover:-translate-x-20 -translate-x-10 m-6">
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
-            </div>
-            <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
-            <div className="icon text-4xl">
-                <ImBlogger2 />
-            </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
+            </div>
+            <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
+            <div className="icon text-4xl">
+                <ImBlogger2 />
+            </div>
+            <p>Blogger</p>
             </div>
     
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
     
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
     
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
     
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
     
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
     
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
     
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
     
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
     
         </div>
@@ -94,81 +94,81 @@ const SlidingSection = () => {
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
     
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
     
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
     
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
     
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
     
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
     
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
     
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
     
             <div className="box bg-[#f1f1f1] border border-black flex items-center justify-center flex-col p-4 rounded-md">
             <div className="icon text-4xl">
                 <ImBlogger2 />
             </div>
-            <p>HIi</p>
+            <p>Blogger</p>
             </div>
     
         </div>
